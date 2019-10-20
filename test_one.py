5630ffe4d41cde72099ff293cc3f493cda54ce4d
Avinash Doddi <avinashdoddi@gmail.com>
2019-10-20 10:44:45 -0500

    remove_exp files

--- test_one.py
+++
@@ -1,5 +0,0 @@
-import requests
-url = 'https://www.virustotal.com/vtapi/v2/url/report'
-params = {'apikey': '5f522cd287653f78b1378fceb8929a938bcf8012f524e008301cc7fb3ec4a533', 'resource':"http://two"}
-response = requests.get(url, params=params)
-print(response.json())
